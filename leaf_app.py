106a48b2ca6bfb81f55e6fc6856e93d129e351fe
Jesse Villines <jesservillines@gmail.com>
2021-02-19 17:05:06 -0700

    save

--- leaf_app.py
+++ leaf_app.py
@@ -117,15 +117,14 @@ def health():
     st.subheader("Take photo of a leaf with your camera and upload here.")
     uploaded_file = st.file_uploader("Upload an image", type = "jpg")
     if uploaded_file is not None:
-    #     image = Image.open(uploaded_file)
-    #     st.image(image, use_column_width=True)
-    #     st.write("")
-    #     name = "temp1.jpg"
-    #
-    #     image.save(datapath+name)
-    #
-    #     result = model_predict(datapath+name, leaf_model)
-        result = model_predict(uploaded_file, leaf_model)
+        image = Image.open(uploaded_file)
+        st.image(image, use_column_width=True)
+        st.write("")
+        name = "temp1.jpg"
+
+        image.save(datapath+name)
+
+        result = model_predict(datapath+name, leaf_model)
         pred = healthType[result]
         st.header("Your leaf is - "+ pred )
         st.subheader("The suggested recovery plan for "+ pred + " is: "+ suggestions[pred])
