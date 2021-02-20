9cde0881e147ad7276aae0441930542083719bf0
Jesse Villines <jesservillines@gmail.com>
2021-02-19 20:05:11 -0700

    m

--- leaf_app.py
+++ leaf_app.py
@@ -107,15 +107,6 @@ def main():
 
 
 
-
-
-
-
-
-
-
-
-
 def health():
     st.title("Check the health of your plant")
     set_png_as_page_bg(datapath+'image_1.jpg')
@@ -123,17 +114,28 @@ def health():
     st.set_option('deprecation.showfileUploaderEncoding', True)
     st.subheader("Take photo of a leaf with your camera and upload here.")
     uploaded_file = st.file_uploader("Upload an image", type = ['jpg', 'png'])
-    if uploaded_file is not None:
-        #image = load_img(image_path,target_size=(250,250))
-        image = Image.open(uploaded_file)
-        st.image(image, width = 250, height = 250)
-        image = img_to_array(image)
-        image = image/255
-        image = np.expand_dims(image,axis=0)
-        result = np.argmax(leaf_model.predict(image))
-        pred = healthType[result]
-        st.header("The state of your leaf is - "+ pred )
-        st.subheader("The suggested recovery plan for "+ pred + " is: "+ suggestions[pred])
+    if img_file_buffer is not None:
+        image = Image.open(img_file_buffer)
+        img_array = np.array(image) # if you want to pass it to OpenCV
+        
+        st.image(
+        image,
+        caption=f"You amazing image has shape {img_array.shape[0:2]}",
+        use_column_width=True,
+    )
+
+####################################################################################################################### SHAPE ERROR
+    # if uploaded_file is not None:
+    #     #image = load_img(image_path,target_size=(250,250))
+    #     image = Image.open(uploaded_file)
+    #     st.image(image, width = 250, height = 250)
+    #     image = img_to_array(image)
+    #     image = image/255
+    #     image = np.expand_dims(image,axis=0)
+    #     result = np.argmax(leaf_model.predict(image))
+    #     pred = healthType[result]
+    #     st.header("The state of your leaf is - "+ pred )
+    #     st.subheader("The suggested recovery plan for "+ pred + " is: "+ suggestions[pred])
 
 #######################################################################################################################
         # image = Image.open(uploaded_file)
